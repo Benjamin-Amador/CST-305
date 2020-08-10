a32ce6e6ebde53ab7d5b5bd437dd55491a417037
Benjamin-Amador <54571512+Benjamin-Amador@users.noreply.github.com>
2020-08-09 18:04:10 -0700

    Add files via upload

---
+++ Project 8/Project 8.py
@@ -0,0 +1,84 @@
+import math
+import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+def riemann_sum(f, a, b, N, method):
+    '''Compute the Riemann sum of f(x) over the interval [a,b].
+
+    Parameters
+    ----------
+    f : function
+        Vectorized function of one variable
+    a , b : numbers
+        Endpoints of the interval [a,b]
+    N : integer
+        Number of subintervals of equal length in the partition of [a,b]
+    method : string
+        Determines the kind of Riemann sum:
+        right : Riemann sum using right endpoints
+        left : Riemann sum using left endpoints
+        midpoint (default) : Riemann sum using midpoints
+
+    Returns
+    -------
+    float
+        Approximation of the integral given by the Riemann sum.
+    '''
+    dx = (b - a)/N
+    x = np.linspace(a, b, N+1)
+
+    if method == 'left':
+        x_left = x[:-1]
+        return np.sum(f(x_left)*dx)
+    elif method == 'right':
+        x_right = x[1:]
+        return np.sum(f(x_right)*dx)
+    elif method == 'midpoint':
+        x_mid = (x[:-1] + x[1:])/2
+        return np.sum(f(x_mid)*dx)
+    else:
+        raise ValueError("Method must be 'left', 'right' or 'midpoint'.")
+
+f = lambda x : (x ** 2) - (x ** 3) # This is where the function will be inputted
+a = -1 #Lower bound of Integral
+b = 0 #Upper bound of Integral
+N = 10 #Width of bars for bar graph
+n = 100 # n in the equation
+x = np.linspace(a, b, N+1)
+y = f(x)
+X = np.linspace(a, b, n*N+1)
+Y = f(X)
+
+print("Riemann sum right: ", riemann_sum(f, a, b, n, 'right'))
+print("Riemann sum middle: ", riemann_sum(f, a, b, n, 'midpoint'))
+print("Riemann sum left: ", riemann_sum(f, a, b, n, 'left'))
+
+plt.figure(figsize=(15,5))
+
+plt.subplot(1,3,1)
+plt.plot(X,Y,'b')
+x_left = x[:-1] # Left endpoints
+y_left = y[:-1]
+plt.plot(x_left,y_left,'b.',markersize=10)
+plt.bar(x_left,y_left,width=(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+plt.title('Left Riemann Sum, N = {}'.format(N))
+
+plt.subplot(1,3,2)
+plt.plot(X,Y,'b')
+x_mid = (x[:-1] + x[1:])/2 # Midpoints
+y_mid = f(x_mid)
+plt.plot(x_mid,y_mid,'b.',markersize=10)
+plt.bar(x_mid,y_mid,width=(b-a)/N,alpha=0.2,edgecolor='b')
+plt.title('Midpoint Riemann Sum, N = {}'.format(N))
+
+plt.subplot(1,3,3)
+plt.plot(X,Y,'b')
+x_right = x[1:] # Left endpoints
+y_right = y[1:]
+plt.plot(x_right,y_right,'b.',markersize=10)
+plt.bar(x_right,y_right,width=-(b-a)/N,alpha=0.2,align='edge',edgecolor='b')
+plt.title('Right Riemann Sum, N = {}'.format(N))
+
+plt.show()
